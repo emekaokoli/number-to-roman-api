a68f3693db829be8f500b67b90b22744c4066b5f
Emeka Okoli <emekaokoli00@gmail.com>
2022-04-04 21:13:11 +0100

    updated endpoint

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/v1/romans', romans);
+app.use('/api/v1/roman', romans);
 app.use(
   '/api/v1/documentation',
   swaggerUi.serve,
